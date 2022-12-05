48c1686422b0a3b4523c57f3b27a5b211cf31c5d
Ali Alperen Derici <42701925+alperenderici@users.noreply.github.com>
2022-12-06 00:06:11 +0300

    Homework1

---
+++ flutter_bootcamp/lib/homeworks/homework1.dart
@@ -0,0 +1,136 @@
+void main() {
+  String city = "Izmir";
+  print(city);
+
+  String country = "Turkey";
+  print(country);
+
+  String phone = "905555555";
+  print(phone);
+
+  int postCode = 33535;
+  print(postCode);
+
+  String email = "email@email.com";
+  print(email);
+
+  String job = "Developer";
+  print(job);
+
+  int stock = 500;
+  print(stock);
+
+  String clientName = "Client1";
+  print(clientName);
+
+  DateTime birthDay = DateTime(2022, 12, 05);
+  print(birthDay);
+
+  int salary = 15000;
+  print(salary);
+
+  bool isMarry = false;
+  print(isMarry);
+
+  String comment = "Good condition";
+  print(comment);
+
+  String paymentDate = "01-11-2022";
+  print(paymentDate);
+
+  String payment = "USD";
+  print(payment);
+
+  int orderQuantity = 99;
+  print(orderQuantity);
+
+  String carModel = "Taycan";
+  print(carModel);
+
+  String bookName = "Be More Yoda";
+  print(bookName);
+
+  DateTime releaseDate = DateTime(05, 12, 2022);
+  print(releaseDate);
+
+  int discountAmount = 50;
+  print(discountAmount);
+
+  int roomAmount = 5;
+  print(roomAmount);
+
+  double latitude = 38.418900;
+  print(latitude);
+
+  double longitude = 27.128700;
+  print(longitude);
+
+  String productName = "Keyboard";
+  print(productName);
+
+  double foodPrice = 99.99;
+  print(foodPrice);
+
+  String brand = "Porsche";
+  print(brand);
+
+  String musicName = "Out of Space";
+  print(musicName);
+
+  double videoTime = 10.34;
+  print(videoTime);
+
+  int productRating = 5;
+  print(productRating);
+
+  String paintName = "Les Demoiselles d'Avignon";
+  print(paintName);
+
+  String folderType = ".dmg";
+  print(folderType);
+
+  String color = "black";
+  print(color);
+
+  String colorCode = "#FFFFFF";
+  print(colorCode);
+
+  String phoneModel = "pay phone";
+  print(phoneModel);
+
+  String screenSize = "1920x1080";
+  print(screenSize);
+
+  double weight = 55.50;
+  print(weight);
+
+  String nationalDay = "Republic Day";
+  print(nationalDay);
+
+  String vacationDay = "01.01.2023";
+  print(vacationDay);
+
+  DateTime booking = DateTime(2022, 05, 12);
+  print(booking);
+
+  String streetName = "Bornova";
+  print(streetName);
+
+  int busLine = 963;
+  print(busLine);
+
+  double minutesLeft = 00.25;
+  print(minutesLeft);
+
+  String trackingCode = "ASD234523SDFSDxXX";
+  print(trackingCode);
+
+  DateTime couponDuration = DateTime(2022, 12, 31, 23, 59, 59);
+  print(couponDuration);
+
+  String couponCode = "PROMO666";
+  print(couponCode);
+
+  String address = "9999 sk. no:99 Izmir";
+  print(address);
+}
