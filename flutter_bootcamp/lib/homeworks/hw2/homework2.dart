6a90d32e683d83be1749eda645728620b3d49f79
Ali Alperen Derici <42701925+alperenderici@users.noreply.github.com>
2022-12-08 14:54:37 +0300

    Homework2 submit

--- flutter_bootcamp/lib/homeworks/hw2/homework2.dart
+++ flutter_bootcamp/lib/homeworks/hw2/homework2.dart
@@ -17,4 +17,42 @@ class Homework2 {
     }
     print(res1 + res2);
   }
+
+  int parkingFee(int time) {
+    int calc = 0;
+    if (time <= 1) {
+      calc += 50;
+    } else {
+      calc = 50 + (time - 1) * 10;
+    }
+    return calc;
+  }
+
+  double celciusToFahrenhiet(int c) {
+    double f = (c * 1.8) + 32;
+    return f;
+  }
+
+  void perimeter(int edge1, int edge2) {
+    int calc = 2 * (edge1 + edge2);
+    print(calc);
+  }
+
+  int factorCalc(int num) {
+    int calc = 1;
+    if (num == 1 || num == 0) {
+      calc = 1;
+    } else {
+      for (int i = num; i >= 1; i--) {
+        calc *= i;
+      }
+    }
+    return calc;
+  }
+
+  void aFinder(String word) {
+    int low = "a".allMatches(word).length;
+    int up = "A".allMatches(word).length;
+    print(low + up);
+  }
 }
--- flutter_bootcamp/lib/homeworks/hw2/homework2_main.dart
+++ flutter_bootcamp/lib/homeworks/hw2/homework2_main.dart
@@ -3,8 +3,21 @@ import 'package:flutter_bootcamp/homeworks/hw2/homework2.dart';
 void main() {
   var hw2 = Homework2();
 
-  // double res = hw2.interiorAngleCalculator(3);
-  // print(res);
+  double res = hw2.interiorAngleCalculator(3);
+  print(res);
 
   hw2.salary(20);
+
+  int park = hw2.parkingFee(3);
+  print(park);
+
+  double convertedC = hw2.celciusToFahrenhiet(30);
+  print(convertedC);
+
+  hw2.perimeter(10, 5);
+
+  int factor = hw2.factorCalc(4);
+  print(factor);
+
+  hw2.aFinder("Alperen");
 }
